6881225fb5c31a932a1f31d31cca3376c1d211d7
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-09-26 12:08:53 +0800

    加入程式

---
+++ TUMT_AI_2D/Assets/程式/Fox.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour     //類別
+{
+    public int speed = 50;          //
+    public float jump = 2.5f;
+    public string foxname = "狐狸";
+    public bool pass = false;
+
+ 
+    private void Start()             //開始事件       
+    {
+        Debug.Log("////");
+    }
+}
