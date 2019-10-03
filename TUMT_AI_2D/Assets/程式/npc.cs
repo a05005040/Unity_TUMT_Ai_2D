f81af135f72ff72064b9b3267fec91688d4c7c2b
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-10-03 11:59:25 +0800

    我做了NPC

---
+++ TUMT_AI_2D/Assets/程式/npc.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class npc : MonoBehaviour
+{
+   
+    public string say_start = "";
+    public string say_complete = "";
+    public string complete = "";
+    [Header("道具")]
+    public string prop_name = "";
+    [Header("對話速度")]
+    public int speed = 50;
+    [Header("任務相關")]
+    public bool pass = false;
+    public int Count_player = 0;
+    public int Count_finish = 10;
+
+
+}
