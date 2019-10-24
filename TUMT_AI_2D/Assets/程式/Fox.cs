0b274a368e2ed5c84b36688d2babb02c302b0c8e
unknown <a05005040@gmail.com>
2019-10-24 11:28:01 +0800

    狐狸鍵盤控制

--- TUMT_AI_2D/Assets/程式/Fox.cs
+++ TUMT_AI_2D/Assets/程式/Fox.cs
@@ -6,10 +6,19 @@ public class Fox : MonoBehaviour     //類別
     public float jump = 2.5f;
     public string foxname = "狐狸";
     public bool pass = false;
+    private Rigidbody2D r2d;
+    
 
- 
+
     private void Start()             //開始事件       
     {
-        Debug.Log("////");
+        r2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        //r2d.AddForce(new Vector2(speed, 0));
+        float horizontal = Input.GetAxis("Horizontal"); //A D 左右
+        transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);//A D 左右
     }
 }
