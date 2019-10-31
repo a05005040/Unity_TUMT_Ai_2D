dd65d0b4b692680d14a12745d4f9edb1e8b304c3
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-10-31 12:15:44 +0800

    對話1031

--- TUMT_AI_2D/Assets/程式/Fox.cs
+++ TUMT_AI_2D/Assets/程式/Fox.cs
@@ -8,6 +8,7 @@ public class Fox : MonoBehaviour     //類別
     public bool pass = false;
     private Rigidbody2D r2d;
     private Transform tra;
+    public bool isGround;
 
 
     private void Start()             //開始事件       
@@ -17,12 +18,43 @@ public class Fox : MonoBehaviour     //類別
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D)) tra.eulerAngles = new Vector3(0, 0, 0);
-        if (Input.GetKeyDown(KeyCode.A)) tra.eulerAngles = new Vector3(0, 180, 0);       //翻轉角度
+        if (Input.GetKeyDown(KeyCode.D)) Turn(0);
+        if (Input.GetKeyDown(KeyCode.A)) Turn(180);
+    }
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
+        r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
     }
     private void FixedUpdate()
     {
+        Jump();
+        Walk();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        Debug.Log("碰到" + collision.gameObject);
 
-        r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
+    }
+
+    /// <summary>
+    /// 轉彎
+    /// </summary>
+    /// <param name="Direction">方向 左轉為180 右轉為0</param>
+    private void Turn(int Direction)
+    {
+
+        tra.eulerAngles = new Vector3(0, Direction, 0);
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&isGround==true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0, jump));
+        }
     }
 }
