8f3493580702a3b345814295eaf62dc44f3f665f
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-11-14 12:07:44 +0800

    無修改

--- TUMT_AI_2D/Assets/程式/npc.cs
+++ TUMT_AI_2D/Assets/程式/npc.cs
@@ -1,12 +1,14 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class npc : MonoBehaviour
 {
-   
+    #region 欄位
     public string say_start = "";
     public string say_complete = "";
     public string complete = "";
+    public state _state;
     [Header("道具")]
     public string prop_name = "";
     [Header("對話速度")]
@@ -15,6 +17,61 @@ public class npc : MonoBehaviour
     public bool pass = false;
     public int Count_player = 0;
     public int Count_finish = 10;
+    [Header("介面")]
+    public GameObject objCanvas;
+    public Text textSay;
+    #endregion
+    public enum state
+    {
+        normal, notComple, comple
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "狐狸")
+        {
+            Say();
+        }
+    
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "狐狸")
+            SayClose();
+    }
+    // <summary>
+    /// 對話：打字效果
+    /// </summary>
+    private void Say()
+    {
+        // 畫布.顯示
+        objCanvas.SetActive(true);
+        // 文字介面.文字 = 對話1
+        textSay.text = say_start;
+    }
 
-
+    /// <summary>
+    /// 關閉對話
+    /// </summary>
+    private void SayClose()
+    {
+        objCanvas.SetActive(false);
+    }
+    private void Start()
+    {
+        switch (_state)
+        {
+            case state.normal:
+                textSay.text = say_start;
+                break;
+            case state.notComple:
+                textSay.text = say_complete;
+                break;
+            case state.comple:
+                textSay.text = complete;
+                break;
+            default:
+                break;
+        }
+    }
 }
